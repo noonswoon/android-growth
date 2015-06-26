7331dd1ae8dee04294f9cde46f73579d2d118235
supanutchow <supanut.ch55@cbs.chula.ac.th>
2015-06-26 12:55:44 +0700

    Prototype version of App "WhyDoPeopleLikeYou"

---
+++ WhyDoPeopleLikeYou_Prototype/app/src/main/java/com/noonswoonapp/moonswoon/SplashScreen.java
@@ -0,0 +1,46 @@
+package com.noonswoonapp.moonswoon;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    private Handler handler;
+    private Runnable runnable;
+    private long mDelayTime;
+    private long mTime = 3000;
+
+    public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+    }
+
+    public void onResume() {
+        super.onResume();
+        mDelayTime = mTime;
+        handler.postDelayed(runnable, mDelayTime);
+        mTime = System.currentTimeMillis();
+    }
+
+    public void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable);
+        mTime = mDelayTime - (System.currentTimeMillis() - mTime);
+    }
+
+}
